942f094f8ba0e4cb8c6296f183c65074bd8ae212
Arian Akbari <akbariarian@gmail.com>
2022-02-19 13:01:11 +0330

    Fixed seed script.

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,51 +1,33 @@
-import mongoose from "mongoose";
-import _ from "lodash";
-import { Profile } from "../models/Profile";
-import { Simulator } from "../models/Simulator";
-import { Favorite } from "../models/Favorite";
-import { DBURL } from "../config";
+import mongoose from "../databases/mongodb";
+import Profile from "../models/Profile";
+import Favorite from "../models/Favorite";
+import Simulator from "../models/Simulator";
 
-(async () => {
-
-  mongoose.connect(DBURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  const profile = new Profile({
-    name: `String`,
-    email: `String`,
-    capital: `123`,
-    divisa: `String`,
-    prefered_cryptocurrency: `String`,
-  });
-  await profile.save();
-
-  const query = { _id: "6093abb3dfd9da1deeae56f2" };
-  const idProfile = await Profile.findOne(query).then((e) => {
-    return e?._id;
-  });
-
-  const simulator = new Simulator({
-    profile_id: idProfile,
-    name: `String`,
-    start_date: `01/05/2021`,
-    check_date: `01/05/2021`,
-    cryptocurrency: `String`,
-    divisa: `String`,
-    Crypto_price_start: `123`,
-    Crypto_price_check: `123`,
-  });
-  await simulator.save();
-
-  const favorite = new Favorite({
-    profile_id: idProfile,
-    name: `String`,
-    favorite1: `String`,
-    favorite2: `String`,
-    favorite3: `String`,
-  });
-  await favorite.save();
-
-  mongoose.disconnect();
-})();
+const seed = async () => {
+    const profileId = new mongoose.Types.ObjectId();
+    await Promise.all([
+        Profile.create({
+            _id: profileId,
+            name: 'Arian',
+            nickname: 'arianak',
+            email: 'test@test.com',
+            divisa: 'divisa',
+            capital: 123,
+            preferredCryptocurrency: 'ETH',
+        }),
+        Simulator.create({
+            profile: profileId,
+            dateRecorded: new Date(),
+            cryptocurrency: 'BTC',
+            euros: 10000,
+            price: 1000,
+            quantity: 10,
+        }),
+        Favorite.create({
+            profile: profileId,
+            name: 'favorite',
+            favorites: ['favorite1', 'favorite2', 'favorite3']
+        })
+    ]);
+}
+seed().then(() => console.log('Seed successful')).catch(e => console.log('Seed failed', e))
